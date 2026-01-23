def83d2fe294422a7d6868c72919f7e8232a6bc6
Rob Simmons <robsimmons@gmail.com>
2026-01-23 14:38:37 -0500

    Fix PORT

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@
 
 import { app } from './app.ts';
 
-const PORT = parseInt(process.env.PORT || '8000');
-app.listen(8000, () => {
+const PORT = parseInt(process.env.PORT || '3000');
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
